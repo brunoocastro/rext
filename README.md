45839b7601248dd60f3d004a1b9731e0851adf1f
BrunooCastro <bruno.scastro2012@hotmail.com>
2020-07-10 11:59:28 -0300

    Updating readme

--- README.md
+++ README.md
@@ -1,13 +1,40 @@
 # REXT
 
-## Text Remover
+*Text Remover Library*
 
 
 A Python library to remove text from images
 
 
-    * V 1.0
+    - V 1.0
+
+
 First version of this software thats object is remove text from specific images of an competition dataset
 
+## Uses
 
- - Use rext.py -h to see how to use
+### As Script:
+    - Use rext.py -h to see how to use this script
+
+
+    - You can call this script by terminal with this command line:
+        -  OBS: Without '<' '>'
+
+        ```shell
+        python3 rext.py -i <input_image>
+        ```
+        
+
+### AS Library:
+    - For library uses, import this Library and call the clean function passing an image as an argument. The return is a clean image.
+
+        - Params: clean(image, show)
+        - If show = True, you can see all processing
+
+        ```python
+        import rext.py
+
+        img = X
+
+        new_img = rext.clean(img)
+        ```
--- rext.py
+++ rext.py
@@ -1,14 +1,39 @@
 #!/usr/bin/python
 
+'''
 ###########################################
-# REXT - V 1.0
-# Text Remover
-# First version of this software
-# thats object is remove text from
-# specific images of an competition dataset
+    REXT - V 1.0
+Text Remover Library
+A Python library to remove text from images
 
-# Use rext.py -h to see how to use
-###########################################
+First version of this software thats object is remove
+text from specific images of an competition dataset
+
+Use as Script:
+- Use rext.py -h to see how to use this script
+
+
+- You can call this script by terminal with this command line:
+    -  OBS: Without '<' '>'
+
+    python3 rext.py -i <input_image>
+
+        
+
+Use as Library:
+- For library uses, import this Library and call the clean function
+passing an image as an argument. The return is a clean image.
+
+    - Params: clean(image, show)
+    - If show = True, you can see all processing
+
+    import rext.py
+
+    img = X
+
+    new_img = rext.clean(img)
+
+'''
 
 import numpy as np
 from matplotlib import pyplot as plt
